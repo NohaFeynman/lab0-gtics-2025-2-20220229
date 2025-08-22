02e318e814861dcf31cea85a71995eb025a617d3
Nilton Condori <a20220229@pucp.edu.pe>
2025-08-21 20:27:20 -0500

    feat: Impresion de lista

--- src/main/java/com/pucp/lab0_2025_2/Lab020252Application.java
+++ src/main/java/com/pucp/lab0_2025_2/Lab020252Application.java
@@ -3,11 +3,19 @@ package com.pucp.lab0_2025_2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class Lab020252Application {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(Lab020252Application.class, args);
+		SpringApplication.run(Lab020252Application.class, args);
+        System.out.println("Hola Mundo");
+        List<String> lista = List.of("Elemento1", "Elemento2", "Elemento3");
+        for (String s : lista) {
+            System.out.println("Elemento "+ s);
+        }
 	}
 
 }
