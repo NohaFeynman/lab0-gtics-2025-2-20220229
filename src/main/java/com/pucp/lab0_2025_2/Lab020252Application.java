e1ff6af90d48320ef071b23e995323e854f800c2
Nilton Condori <a20220229@pucp.edu.pe>
2025-08-21 20:20:54 -0500

    feat: hola mundo

---
+++ src/main/java/com/pucp/lab0_2025_2/Lab020252Application.java
@@ -0,0 +1,13 @@
+package com.pucp.lab0_2025_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab020252Application {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(Lab020252Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pucp/lab0_2025_2/Lab020252ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucp.lab0_2025_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab020252ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
